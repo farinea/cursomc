bb0314d3b5365b83a7a9a7070a827e167820e4a6
Wagner Farinea <wagnereffe@gmail.com>
2019-03-29 10:20:25 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/farinea/cursomc/dto/ProdutoDTO.java
@@ -0,0 +1,58 @@
+package com.farinea.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.farinea.cursomc.domain.Produto;
+
+public class ProdutoDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	private Double preco;
+
+	public ProdutoDTO( ) {
+		
+	}
+	
+	public ProdutoDTO(Produto obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		preco = obj.getPreco();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+}
