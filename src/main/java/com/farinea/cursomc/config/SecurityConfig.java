2ee0ffe3bac02249a4c0d733f66be804a8527ba3
Wagner Farinea <wagnereffe@gmail.com>
2019-04-03 14:55:00 -0300

    Implementando autorização

--- src/main/java/com/farinea/cursomc/config/SecurityConfig.java
+++ src/main/java/com/farinea/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.farinea.cursomc.security.JWTAuthenticationFilter;
+import com.farinea.cursomc.security.JWTAuthorizationFilter;
 import com.farinea.cursomc.security.JWTUtil;
 
 @Configuration
@@ -66,6 +67,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
