74da9d8701547bf095027dfc548175aafb3f9cf5
Wagner Farinea <wagnereffe@gmail.com>
2019-04-03 15:26:20 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/farinea/cursomc/services/PedidoService.java
+++ src/main/java/com/farinea/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.farinea.cursomc.domain.Cliente;
 import com.farinea.cursomc.domain.ItemPedido;
 import com.farinea.cursomc.domain.PagamentoComBoleto;
 import com.farinea.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.farinea.cursomc.domain.enums.EstadoPagamento;
 import com.farinea.cursomc.repositories.ItemPedidoRepository;
 import com.farinea.cursomc.repositories.PagamentoRepository;
 import com.farinea.cursomc.repositories.PedidoRepository;
+import com.farinea.cursomc.security.UserSS;
+import com.farinea.cursomc.services.exceptions.AuthorizationException;
 import com.farinea.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -69,5 +75,15 @@ public class PedidoService {
 		System.out.println(obj);
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
