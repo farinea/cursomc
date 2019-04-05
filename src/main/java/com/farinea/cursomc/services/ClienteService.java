ddb3d8bba67f14df8445102959b9c64784327829
Wagner Farinea <wagnereffe@gmail.com>
2019-04-05 15:17:37 -0300

    Usando padrão de nomes para imagens e ajustando tamanho das imagens

--- src/main/java/com/farinea/cursomc/services/ClienteService.java
+++ src/main/java/com/farinea/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.farinea.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -43,6 +45,15 @@ public class ClienteService {
 	@Autowired
 	private S3Service s3Service;
 		
+	@Autowired
+	private ImageService imageService;
+
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
+
+	@Value("${img.profile.size}")
+	private Integer size;
+
 	public Cliente find(Integer id) {
 		
 		UserSS user = UserService.authenticated();
@@ -116,16 +127,6 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 
-		URI uri = s3Service.uploadFile(multipartFile); 
-		
-		Cliente cli = find(user.getId());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
-		
-		return uri;
-		
-		/*
-		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
 		jpgImage = imageService.cropSquare(jpgImage);
 		jpgImage = imageService.resize(jpgImage, size);
@@ -133,7 +134,6 @@ public class ClienteService {
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
-		*/
 	}
 
 }
